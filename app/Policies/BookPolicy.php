32c693aade7eddc37163ce7dd29db0cc520b3c6f
abdulrhman <abdulrhmanalsead@gmail.com>
2025-07-15 10:53:04 +0300

    fix BookPlicy to admin

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -4,35 +4,29 @@ namespace App\Policies;
 
 use App\Models\User;
 use App\Models\Book;
+use App\Enums\Role;
 
 class BookPolicy
 {
-
     public function create(User $user): bool
     {
-        return $user->role === 'admin';
+        return $user->role === Role::Admin;
     }
-
 
     public function update(User $user, Book $book): bool
     {
-        return $user->role === 'admin';
+        return $user->role === Role::Admin;
     }
-
-
 
     public function delete(User $user, Book $book): bool
     {
-        return $user->role == 'admin';
+        return $user->role === Role::Admin;
     }
-
-
 
     public function view(User $user, Book $book): bool
     {
         return true;
     }
-
 
     public function viewAny(User $user): bool
     {
